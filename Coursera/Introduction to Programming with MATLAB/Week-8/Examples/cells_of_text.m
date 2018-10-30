299cecee46a28f982ca33116e885d4e836413764
MrinmoiHossain <mokit.aust@gmail.com>
2018-10-30 18:39:14 +0600

    week-8 of matlab

---
+++ Coursera/Introduction to Programming with MATLAB/Week-8/Examples/cells_of_text.m
@@ -0,0 +1,12 @@
+%% The Ultimate Legend of Big John
+page{1} = 'You could find him on the field almost any day.';
+page{2} = 'Tall, dark hair, and eyes of steel gray.';
+page{3} = 'They say he pulled a Frisbee ''bout half a mile,';
+page{4} = 'And when he''d stick in the corner, you could almost catch a smile';
+page{5} = 'On Big John.';
+%% Print it
+fprintf('\n');
+for ii = 1:length(page)
+    fprintf('%s\n',page{ii});
+end
+fprintf('\n');
---
+++ Coursera/Introduction to Programming with MATLAB/Week-8/Examples/char_codes.m
@@ -0,0 +1,5 @@
+function char_codes
+for ii = 33:126
+    fprintf('%s',ii);
+end
+fprintf('%s',10);
