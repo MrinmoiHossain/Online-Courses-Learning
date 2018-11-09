5eabf10ade652956520efcc5b33afcf769bc3a88
MrinmoiHossain <mokit.aust@gmail.com>
2018-11-09 12:05:51 +0600

    python -capstone

--- Coursera/Capstone Retrieving, Processing, and Visualizing Data with Python/Week-2/Example/Page_Rank/spider.py
+++ Coursera/Capstone Retrieving, Processing, and Visualizing Data with Python/Week-2/Example/Page_Rank/spider.py
@@ -40,7 +40,7 @@ else:
         starturl = starturl[:-1]
     web = starturl
 
-    if web.starturl('.htm') or starturl.endswith('html'):
+    if starturl.endswith('.htm') or starturl.endswith('.html'):
         pos = starturl.rfind('/')
         web = starturl[:pos]
 
@@ -78,3 +78,81 @@ while True:
         break
 
     print(fromid, url, end=' ')
+
+    # If we are retrieving this page, there should be no links from it
+    cur.execute('DELETE from Links WHERE from_id=?', (fromid,))
+    try:
+        document = urlopen(url, context=ctx)
+
+        html = document.read()
+        if document.getcode() != 200:
+            print("Error on page: ", document.getcode())
+            cur.execute('UPDATE Pages SET error=? WHERE url=?', (document.getcode(), url))
+
+        if 'text/html' != document.info().get_content_type():
+            print("Ignore non text/html page")
+            cur.execute('DELETE FROM Pages WHERE url=?', (url,))
+            cur.execute('UPDATE Pages SET error=0 WHERE url=?', (url,))
+            con.commit()
+            continue
+
+        print('(' + str(len(html)) + ')', end=' ')
+
+        soup = BeautifulSoup(html, "html.parser")
+    except KeyboardInterrupt:
+        print('')
+        print('Program interrupted by user...')
+        break
+    except:
+        print("Unable to retrieve or parse page")
+        cur.execute('UPDATE Pages SET error=-1 WHERE url=?', (url,))
+        con.commit()
+        continue
+
+    cur.execute('INSERT OR IGNORE INTO Pages(url, html, new_rank) VALUES(?, NULL, 1.0)', (url,))
+    cur.execute('UPDATE Pages SET html=? WHERE url=?', (memoryview(html), url))
+    con.commit()
+
+    # Retrieve all of the anchor tags
+    tags = soup('a')
+    count = 0
+    for tag in tags:
+        href = tag.get('href', None)
+        if ( href is None ) : continue
+        # Resolve relative references like href="/contact"
+        up = urlparse(href)
+        if ( len(up.scheme) < 1 ) :
+            href = urljoin(url, href)
+        ipos = href.find('#')
+        if ( ipos > 1 ) : href = href[:ipos]
+        if ( href.endswith('.png') or href.endswith('.jpg') or href.endswith('.gif') ) : continue
+        if ( href.endswith('/') ) : href = href[:-1]
+        # print href
+        if ( len(href) < 1 ) : continue
+
+		# Check if the URL is in any of the webs
+        found = False
+        for web in webs:
+            if ( href.startswith(web) ) :
+                found = True
+                break
+        if not found : continue
+
+        cur.execute('INSERT OR IGNORE INTO Pages (url, html, new_rank) VALUES ( ?, NULL, 1.0 )', ( href, ) )
+        count = count + 1
+        con.commit()
+
+        cur.execute('SELECT id FROM Pages WHERE url=? LIMIT 1', ( href, ))
+        try:
+            row = cur.fetchone()
+            toid = row[0]
+        except:
+            print('Could not retrieve id')
+            continue
+        # print fromid, toid
+        cur.execute('INSERT OR IGNORE INTO Links (from_id, to_id) VALUES ( ?, ? )', ( fromid, toid ) )
+
+
+    print(count)
+
+cur.close()
