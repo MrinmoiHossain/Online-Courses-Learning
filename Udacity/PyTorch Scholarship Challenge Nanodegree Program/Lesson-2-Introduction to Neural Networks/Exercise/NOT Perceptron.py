863b0641dafb5ca51277df1404552d054b944262
MrinmoiHossain <mokit.aust@gmail.com>
2018-11-16 12:09:49 +0600

    not perception

---
+++ Udacity/PyTorch Scholarship Challenge Nanodegree Program/Lesson-2-Introduction to Neural Networks/Exercise/NOT Perceptron.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+# TODO: Set weight1, weight2, and bias
+weight1 = 1.0
+weight2 = -2.5
+bias = 1.0
+
+
+# DON'T CHANGE ANYTHING BELOW
+# Inputs and outputs
+test_inputs = [(0, 0), (0, 1), (1, 0), (1, 1)]
+correct_outputs = [True, False, True, False]
+outputs = []
+
+# Generate and check output
+for test_input, correct_output in zip(test_inputs, correct_outputs):
+    linear_combination = weight1 * test_input[0] + weight2 * test_input[1] + bias
+    output = int(linear_combination >= 0)
+    is_correct_string = 'Yes' if output == correct_output else 'No'
+    outputs.append([test_input[0], test_input[1], linear_combination, output, is_correct_string])
+
+# Print output
+num_wrong = len([output[4] for output in outputs if output[4] == 'No'])
+output_frame = pd.DataFrame(outputs, columns=['Input 1', '  Input 2', '  Linear Combination', '  Activation Output', '  Is Correct'])
+if not num_wrong:
+    print('Nice!  You got it all correct.\n')
+else:
+    print('You got {} wrong.  Keep trying!\n'.format(num_wrong))
+print(output_frame.to_string(index=False))
