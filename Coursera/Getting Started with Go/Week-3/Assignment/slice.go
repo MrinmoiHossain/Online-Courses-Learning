118fd85b68f931b28999bea4fc2849e408c5f546
MrinmoiHossain <mokit.aust@gmail.com>
2018-12-27 14:53:38 +0600

    assignment of week-3 of go

---
+++ Coursera/Getting Started with Go/Week-3/Assignment/slice.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+)
+
+func main(){
+	sl := make([]int, 3)
+	cn := 3
+
+	for{
+		fmt.Printf("Enter an interger (X - quit): ")
+		var n string
+		fmt.Scan(&n)
+
+		if(n == "X"){
+			break
+		}else {
+			t,_ := strconv.Atoi(n)
+			sl = append(sl, t)
+			cn++
+		}
+	}
+	sort.Ints(sl[3:cn])
+
+	fmt.Printf("Contents of the slice are: ")
+	for i := 3; i < len(sl); i++{
+		fmt.Printf("%d ", sl[i])
+	}
+	fmt.Println()
+}
