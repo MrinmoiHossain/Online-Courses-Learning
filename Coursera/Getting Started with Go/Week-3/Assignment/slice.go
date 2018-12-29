b7e631deebb815e15c730852fa20a773d16cbe28
MrinmoiHossain <mokit.aust@gmail.com>
2018-12-29 22:13:02 +0600

    bug free in slice.go

--- Coursera/Getting Started with Go/Week-3/Assignment/slice.go
+++ Coursera/Getting Started with Go/Week-3/Assignment/slice.go
@@ -11,7 +11,7 @@ func main(){
 	cn := 3
 
 	for{
-		fmt.Printf("Enter an interger (X - quit): ")
+		fmt.Printf("Enter value (X - quit): ")
 		var n string
 		fmt.Scan(&n)
 
@@ -22,12 +22,13 @@ func main(){
 			sl = append(sl, t)
 			cn++
 		}
-	}
-	sort.Ints(sl[3:cn])
 
-	fmt.Printf("Contents of the slice are: ")
-	for i := 3; i < len(sl); i++{
-		fmt.Printf("%d ", sl[i])
+		sort.Ints(sl[3:cn])
+
+		fmt.Printf("Contents of the slice are: ")
+		for i := 3; i < len(sl); i++{
+			fmt.Printf("%d ", sl[i])
+		}
+		fmt.Println()
 	}
-	fmt.Println()
 }
