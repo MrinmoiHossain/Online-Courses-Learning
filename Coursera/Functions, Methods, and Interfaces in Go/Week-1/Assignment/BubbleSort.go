e9c55b1392deeb55a9d80934207e5ab9a016979d
MrinmoiHossain <mokit.aust@gmail.com>
2019-01-22 09:00:45 +0600

    week-1 of go programming

---
+++ Coursera/Functions, Methods, and Interfaces in Go/Week-1/Assignment/BubbleSort.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func BubbleSort(){
+
+}
+
+func main(){
+	fmt.Printf("Enter 10 integers value: ")
+	val := [10]int{}
+	for i := 0; i < 10; i++{
+		temp := fmt.Scan()
+		append(val, i)
+	}
+}
---
+++ Coursera/Functions, Methods, and Interfaces in Go/Week-1/Exercise/func.go
@@ -0,0 +1,11 @@
+package  main
+
+import "fmt"
+
+func PrintHello(a int, c int){
+	fmt.Println(a * c)
+}
+
+func main(){
+	PrintHello(4, 6)
+}
